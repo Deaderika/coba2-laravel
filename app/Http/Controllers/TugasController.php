fc6aa54d4101a7328e11ab0984f9d1aaef8b0e0c
deaderika <derikadea87@gmail.com>
2022-09-12 19:56:54 +0700

    mengerjakan tugas

---
+++ app/Http/Controllers/TugasController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\In;
+
+class TugasController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    
+        public function tugasphp(){
+            for ($i=1; $i <= 100 ; $i++) { 
+                if ($i%3 == 0 && $i%5 == 0) {
+                    $p = $i/3;
+                    $l = $i/5;
+
+                    $hasil = $this->luasPersegiPanjang($p, $l);
+                    echo number_format ($hasil, 2). "<br>";
+                }else if ($i%3 == 0) {
+                    $r = $i/3;
+
+                    $hasil = $this->luasLingkaran($r);
+                    echo number_format($hasil, 2). "<br>";
+                }else if ($i%5 == 0 ) {
+                    $r = $i/5;
+
+                    $hasil = $this->kelilingLingkaran($r);
+                    echo number_format($hasil, 0);
+                }else {
+                    echo number_format($i, 2)."<br>";
+                }
+            }
+        }
+        
+        function luasLingkaran(int $r){
+            $luas = 3.14 * $r * $r;
+
+            return $luas;
+        }
+        
+        function kelilingLingkaran(Int $r){
+            $keliling = 2 * 3.14 * $r;
+            
+            return $keliling;
+        }
+
+        function luasPersegiPanjang(int $p, int $l){
+            $luas= $p * $l;
+
+            return $luas;
+        }
+    }
